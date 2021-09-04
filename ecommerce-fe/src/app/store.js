3d6750d76d70d149010e95fc7c8a89d3e41f9b5d
xuandungdoan <xuandung.cnc@gmail.com>
2021-09-04 19:25:43 +0700

    integrate get products

--- ecommerce-fe/src/app/store.js
+++ ecommerce-fe/src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import authReducer from "../features/counter/authSlice";
+import productReducer from "../features/counter/productSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     auth: authReducer,
+    product: productReducer,
   },
 });
